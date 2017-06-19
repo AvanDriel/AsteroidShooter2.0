ee07298ec852c1bde66a4df95980353c2fbff3d4
Maarten Huijser <maarten@huijser.nl>
2017-06-19 19:09:37 +0200

    added main menu animation
    added main menu animation and added the css so its look pretty

--- dev/startScreen.ts
+++ dev/startScreen.ts
@@ -1,19 +1,62 @@
 class startScreen {
     public button: HTMLElement;
+    public title: HTMLElement;
+    public text: HTMLElement;
+    public discription: HTMLElement;
 
     constructor() {
         var audio = new Audio('../docs/sounds/soundtrack.mp3');
         audio.play();   
-        this.button = document.createElement('start_but');
         
+        //create title
+        this.title = document.createElement('title');
+        this.title.innerHTML = 'Welkom bij DE orginele Asteroid Shooter.';
+        document.body.appendChild(this.title);
+
+        // create game discription
+        this.text = document.createElement('text');
+        this.text.innerHTML = 'Om de game te kunnen spelen druk je op start.'
+        document.body.appendChild(this.text);
+
+        //creating discripotion for controls
+        this.discription = document.createElement('text');
+        this.discription.innerHTML = 'Controls: A & D om te bewegen, spatie om te schieten.'
+        document.body.appendChild(this.discription);
+
+        //create start_but div.
+        this.button = document.createElement('start_but');
         this.button.addEventListener("click", () => this.deleteAll());
         document.body.appendChild(this.button);
+
+
+        //Dropdown animation for the title
+        TweenLite.set(this.title, {x:0, y:-450});
+        TweenLite.to(this.title, 1, {y:150, ease:Bounce.easeOut});
+
+        //Dropdown animation for discription text
+        TweenLite.set(this.text, {x:0, y:-500})
+        TweenLite.to(this.text, 1, {y:300, ease:Bounce.easeOut});
+
+        //Dropdown animation for discription controls
+        TweenLite.set(this.discription, {x:0, y:-500})
+        TweenLite.to(this.discription, 1, {y:350, ease:Bounce.easeOut});
+
+        //Dropdown anumation for Button
+        TweenLite.set(this.button, {x:0, y:-400});
+        TweenLite.to(this.button,1, {y:500, ease:Bounce.easeOut});
     }
     deleteAll() {
+        this.text.remove();
+        this.text = undefined;
+        this.discription.remove();
+        this.discription = undefined;
+        this.title.remove();
+        this.title = undefined;
         this.button.remove();
         this.button = undefined;
 
         new Game();
+
 
     }
 
--- docs/js/main.js
+++ docs/js/main.js
@@ -283,11 +283,34 @@ var startScreen = (function () {
         var _this = this;
         var audio = new Audio('../docs/sounds/soundtrack.mp3');
         audio.play();
+        this.title = document.createElement('title');
+        this.title.innerHTML = 'Welkom bij DE orginele Asteroid Shooter.';
+        document.body.appendChild(this.title);
+        this.text = document.createElement('text');
+        this.text.innerHTML = 'Om de game te kunnen spelen druk je op start.';
+        document.body.appendChild(this.text);
+        this.discription = document.createElement('text');
+        this.discription.innerHTML = 'Controls: A & D om te bewegen, spatie om te schieten.';
+        document.body.appendChild(this.discription);
         this.button = document.createElement('start_but');
         this.button.addEventListener("click", function () { return _this.deleteAll(); });
         document.body.appendChild(this.button);
+        TweenLite.set(this.title, { x: 0, y: -450 });
+        TweenLite.to(this.title, 1, { y: 150, ease: Bounce.easeOut });
+        TweenLite.set(this.text, { x: 0, y: -500 });
+        TweenLite.to(this.text, 1, { y: 300, ease: Bounce.easeOut });
+        TweenLite.set(this.discription, { x: 0, y: -500 });
+        TweenLite.to(this.discription, 1, { y: 350, ease: Bounce.easeOut });
+        TweenLite.set(this.button, { x: 0, y: -400 });
+        TweenLite.to(this.button, 1, { y: 500, ease: Bounce.easeOut });
     }
     startScreen.prototype.deleteAll = function () {
+        this.text.remove();
+        this.text = undefined;
+        this.discription.remove();
+        this.discription = undefined;
+        this.title.remove();
+        this.title = undefined;
         this.button.remove();
         this.button = undefined;
         new Game();
